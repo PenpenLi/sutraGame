a54a70f7c0d0385939dc284abee49dc48d89275f
yuqangy@qq.com <yuqangy@qq.com>
2018-01-07 21:29:11 +0800

    1

--- frameworks/runtime-src/proj.android-studio/app/src/org/cocos2dx/lua/adManager.java
+++ frameworks/runtime-src/proj.android-studio/app/src/org/cocos2dx/lua/adManager.java
@@ -17,52 +17,54 @@ public class adManager {
     {
         appActivity = instance;
 
-        admobAD = new adMobUnit();
-        admobAD.initSDK(appActivity);
+        //admobAD = new adMobUnit();
+        //admobAD.initSDK(appActivity);
 
-        inMobiAD = new inMobiUnit();
-        inMobiAD.initSDK(appActivity);
+        //inMobiAD = new inMobiUnit();
+        //inMobiAD.initSDK(appActivity);
 
 
         return  true;
     }
 
     public void luaLoadAd(final String param,final int luaFunc){
-        if (!admobAD.isLoaded())
-            admobAD.luaLoadAd(param, luaFunc);
+       // if (!admobAD.isLoaded())
+           // admobAD.luaLoadAd(param, luaFunc);
 
-        if(!inMobiAD.isLoaded())
-            inMobiAD.luaLoadAd(param, luaFunc);
+        //if(!inMobiAD.isLoaded())
+            //inMobiAD.luaLoadAd(param, luaFunc);
     }
     public void luaShowAd(final String param,final int luaFunc){
-        if(admobAD.isLoaded())
-            admobAD.luaShowAd(param, luaFunc);
+       // if(admobAD.isLoaded())
+          //  admobAD.luaShowAd(param, luaFunc);
 
-        if(inMobiAD.isLoaded())
-            inMobiAD.luaShowAd(param, luaFunc);
+        //if(inMobiAD.isLoaded())
+            //inMobiAD.luaShowAd(param, luaFunc);
     }
     public void luaHideAd(final String param,final int luaFunc){
-        admobAD.luaHideAd(param, luaFunc);
+       // admobAD.luaHideAd(param, luaFunc);
 
-        inMobiAD.luaHideAd(param, luaFunc);
+        //inMobiAD.luaHideAd(param, luaFunc);
     }
     public void luaStateAd(final String param,final int luaFunc){
         admobAD.luaStateAd(param, luaFunc);
 
-        inMobiAD.luaStateAd(param, luaFunc);
+        //inMobiAD.luaStateAd(param, luaFunc);
     }
 
     public void onResume() {
-        admobAD.onResume();
+        //admobAD.onResume();
     }
 
 
     public void onPause() {
-        admobAD.onPause();
+
+        //admobAD.onPause();
     }
 
 
     public void onDestroy() {
-        admobAD.onDestroy();
+
+        //admobAD.onDestroy();
     }
 }
