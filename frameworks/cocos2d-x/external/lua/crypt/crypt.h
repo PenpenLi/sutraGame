6c793c37a08ddaf425bc114bc1ef201e417e047b
yuqangy@qq.com <yuqangy@qq.com>
2018-01-20 12:15:05 +0800

    增加 lua5.3库

---
+++ frameworks/cocos2d-x/external/lua/crypt/crypt.h
@@ -0,0 +1,19 @@
+
+/* luasocket_scripts.h.h */
+
+#ifndef __LUA_CRYPT_H_
+#define __LUA_CRYPT_H_
+
+// #if __cplusplus
+// extern "C" {
+// #endif
+
+#include "lua.h"
+
+extern int luaopen_crypt(lua_State* L);
+
+// #if __cplusplus
+// }
+// #endif
+
+#endif /* __LUA_CRYPT_H_ */
---
+++ frameworks/cocos2d-x/external/lua/stringex/stringex.h
@@ -0,0 +1,17 @@
+
+#ifndef __LUA_STRINGEX_H_
+#define __LUA_STRINGEX_H_
+
+// #if __cplusplus
+// extern "C" {
+// #endif
+
+#include "lua.h"
+
+extern int luaopen_stringex(lua_State* L);
+
+// #if __cplusplus
+// }
+// #endif
+
+#endif /* __LUA_STRINGEX_H_ */
